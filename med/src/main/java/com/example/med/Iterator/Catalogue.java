8342cd86b8c7fb89a0454cc9dd53116b892d0f58
idriss Tagny <idrisstagny@gmail.com>
2026-01-13 14:03:20 +0100

    Refactor Catalogue en interface et ajout de la classe CatalogueVehicule pour implémenter le modèle d'itérateur.

--- med/src/main/java/com/example/med/Iterator/Catalogue.java
+++ med/src/main/java/com/example/med/Iterator/Catalogue.java
@@ -1,19 +1,7 @@
 package com.example.med.Iterator;
-// import com.example.med.Iterator.VehiculeIterator;
 import com.example.med.model.catalogue.Vehicule;
-import java.util.List;
-import java.util.ArrayList;
 
-public class Catalogue {
-    private List<Vehicule> vehicules = new ArrayList<>();
-
-
-    public void addVehicule(Vehicule vehicule) {
-        vehicules.add(vehicule);
-    }
-
-    public VehiculeIterator getIterator() {
-        return new CatalogueIterator(this.vehicules);
-    }
-
+public interface Catalogue {
+    public VehiculeIterator getIterator();
+    public void addVehicule(Vehicule vehicule);
 }
