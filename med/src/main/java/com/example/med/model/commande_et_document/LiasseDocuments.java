0e9d85a701682483d0c9462094d98a9f8ef9d0e0
imelda2627 <imeldaktis@gmail.com>
2026-01-14 11:34:28 +0100

    Refactoring

--- med/src/main/java/com/example/med/model/commande_et_document/LiasseDocuments.java
+++ med/src/main/java/com/example/med/model/commande_et_document/LiasseDocuments.java
@@ -3,24 +3,40 @@ package com.example.med.model.commande_et_document;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-@Data
+@Getter
+@Setter
+
 public class LiasseDocuments {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_liasse_de_documents")
     private Long idLiasse;
 
-    @OneToMany(mappedBy = "liasse", cascade = CascadeType.ALL)
+    
+    @OneToMany(mappedBy = "liasse", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonManagedReference 
     private List<Document> documents = new ArrayList<>();
 
+    
+    
+    @Transient 
+    @JsonIgnore 
     private static LiasseDocuments instance = null;
 
     
-    protected LiasseDocuments() {}
+    public LiasseDocuments() {
+        super();
+    }
 
+    
     public static LiasseDocuments getInstance() {
         if (instance == null) {
             instance = new LiasseDocuments();
@@ -28,8 +44,10 @@ public class LiasseDocuments {
         return instance;
     }
 
-    public void ajouterDocument(Document doc) {
-        this.documents.add(doc);
-        doc.setLiasse(this);
+    
+    public Document ajouterDocument(Document document) {
+        this.documents.add(document);
+        document.setLiasse(this); 
+        return document;
     }
 }
